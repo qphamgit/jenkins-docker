5cb5abd1eb8ae6a1dad01f4737547e8e7f5d0a30
Pham Quynh <thuyquynh.toki@gmail.com>
2020-09-24 12:00:57 -0500

    fix error in createJobs.groovy

--- createJobs.groovy
+++ createJobs.groovy
@@ -1,7 +1,8 @@
-pipelineJob('pipelineJob')
+pipelineJob('pipelineJob') {
     definition {
         cps {
             script(readFileFromWorkspace('pipelineJob.groovy'))
             sandbox()
         }
     }
+}
